f999d478e50a643b9f5f76d7751ddf7d2362a2be
jzakrz <32913649+jzakrz@users.noreply.github.com>
2026-08-29 13:01:12 +0200

    chore: allow Steam cover images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shared.fastly.steamstatic.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
